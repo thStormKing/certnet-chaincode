7e721761f22e59fc54f197ddeb41394a1452066c
thStormKing <ss4kakarot4@gmail.com>
2023-01-11 01:45:46 +0530

    commit

--- chaincode/contract.js
+++ chaincode/contract.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const {Contract} = require('fabric-contract-api');
-const studentKeyObjectType = 'certnet.student';
 
 class CertnetContract extends Contract {
 
@@ -16,35 +15,35 @@ class CertnetContract extends Contract {
     // 1.Create Student
     async createStudent(ctx, studentId, name, email){
         // Composite Keys ->
-        const studentKey = ctx.stub.createCompositeKey(studentKeyObjectType, [studentId]);
+        const studentKey = ctx.stub.createCompositeKey('certnet.student', [studentId]);
         
         const newStudentObject = {
-            docType: 'student',
-            studentId: studentId,
-            studentName: name,
-            studentEmail: email,
-            school: ctx.clientIdentity.getId(),
-            createdAt: ctx.stub.getTxTimestamp(),
-            updatedAt: ctx.stub.getTxTimestamp()
-        }
+			docType: 'student',
+			studentId: studentId,
+			name: name,
+			email: email,
+			school: ctx.clientIdentity.getID(),
+			createdAt: ctx.stub.getTxTimestamp(),
+			updatedAt: ctx.stub.getTxTimestamp()
+		}
         // Convert data to byte array or buffer stream before saving to blockchain
         const studentBuffer = Buffer.from(JSON.stringify(newStudentObject));
         // putState - stores data to the blockchain
-        await ctx.stub.putState(studentId, studentBuffer);
-        return newStudentObject;
-    }
+        await ctx.stub.putState(studentKey, studentBuffer);
+		return newStudentObject;
+    // }
     // 2.Get Student
-    async getStudent(ctx, studentId){
-        // Re-create composite key to fetch the correct student object
-        const studentKey = ctx.stub.createCompositeKey(studentKeyObjectType,[studentId]);
-        // Retrieve value from blockchain. It will return data as byte array
-        const studentBuffer = await ctx.stub.getState(studentKey);
-        if(studentBuffer){
-            return JSON.parse(studentBuffer.toString());
-        } else {
-            return 'Asset with key '+studentId+' does not exist on network';
-        }
-    }
+    // async getStudent(ctx, studentId){
+    //     // Re-create composite key to fetch the correct student object
+    //     const studentKey = ctx.stub.createCompositeKey('certnet.student',[studentId]);
+    //     // Retrieve value from blockchain. It will return data as byte array
+    //     const studentBuffer = await ctx.stub.getState(studentKey);
+    //     if(studentBuffer){
+    //         return JSON.parse(studentBuffer.toString());
+    //     } else {
+    //         return 'Asset with key '+studentId+' does not exist on network';
+    //     }
+    // }
     // 3.Issue Certificate
     // 4.Verify Certificate
 }
