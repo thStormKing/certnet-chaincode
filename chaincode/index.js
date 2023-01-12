a9490fd44f5112bf139b093b4c92a18553641988
thStormKing <ss4kakarot4@gmail.com>
2023-01-13 00:03:04 +0530

    changes

--- chaincode/index.js
+++ chaincode/index.js
@@ -1,4 +1,4 @@
 'use strict'
 
-const certificationContract = require('./contract.js')
-module.exports.contracts = [certificationContract]
+const certificationContract = require('./contract.js').default;
+module.exports.contracts = [certificationContract];
